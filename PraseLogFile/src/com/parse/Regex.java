46fc838a9f5668d9700a8085d46e5942386ed5d5
Rahul Soni <rahul.soni16@gmail.com>
2018-02-01 18:32:34 +0530

    Commit

---
+++ PraseLogFile/src/com/parse/Regex.java
@@ -0,0 +1,10 @@
+package com.parse;
+
+public class Regex {
+    public static void main(String args[]) {
+
+	System.out.println("cat".matches("cat")); // "cat"
+	System.out.println("cat".matches("cat")); // "cat"
+    }
+
+}
